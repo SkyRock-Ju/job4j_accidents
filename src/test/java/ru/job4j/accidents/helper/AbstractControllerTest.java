4635a7737255042e42a57d81f6e72ef25aa97b30
Dulustan <skyrockpk@gmail.com>
2023-06-25 22:43:32 +0700

    tests

--- src/test/java/ru/job4j/accidents/helper/AbstractControllerTest.java
+++ src/test/java/ru/job4j/accidents/helper/AbstractControllerTest.java
@@ -3,9 +3,11 @@ package ru.job4j.accidents.helper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.accidents.AccidentsApplication;
 
+@ActiveProfiles("test")
 @SpringBootTest(classes = AccidentsApplication.class)
 @AutoConfigureMockMvc
 public abstract class AbstractControllerTest {
